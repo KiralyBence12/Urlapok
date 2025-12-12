40da1901f99ec7b0e688a02551bbe61847d61a86
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-12-12 08:44:35 +0100

    Névellenőrzés alertekkel

--- alap_ellenorzes.js
+++ alap_ellenorzes.js
@@ -1,1 +1,23 @@
 // /^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ ]+$/
+
+function ellenorAlap(){
+    let nev = document.forms["alapUrlap"]["nev"].value;
+    console.log(nev);
+
+    // ha üres illetve csak szóközöket tartalmaz
+    if(nev.trim() === ""){
+        alert("A név mező nem lehet üres!");
+        return false;
+    }
+
+    let nevfilter = /^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ ]+$/;
+    if(!nevfilter.test(nev)){
+        alert("A név mező csak betűket és szóközöket tartalmazhat!");
+        return false;
+    }
+
+    let nevKiiratasa = document.getElementById("nevEredmeny");
+    nevKiiratasa.innerText = "Helyes név: " + nev;
+    return false;
+
+}
